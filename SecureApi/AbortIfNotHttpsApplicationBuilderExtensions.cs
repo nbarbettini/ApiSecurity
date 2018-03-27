12a8f4b31eb977714a85929c957a59ed7c0a32f8
Nate Barbettini <nathanaelb@gmail.com>
2018-03-26 21:17:06 -0700

    Add first version

---
+++ SecureApi/AbortIfNotHttpsApplicationBuilderExtensions.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace Recaffeinate.SecureApi
+{
+    public static class AbortIfNotHttpsApplicationBuilderExtensions
+    {
+        public static IApplicationBuilder AbortIfNotHttps(this IApplicationBuilder app)
+        {
+            app.Use(async (context, next) =>
+            {
+                if (!context.Request.IsHttps)
+                {
+                    context.Abort();
+                }
+
+                await next();
+            });
+
+            return app;
+        }
+    }
+}
---
+++ SecureApi/RequireHttpsOrTerminateAttribute.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Recaffeinate.SecureApi
+{
+    public class RequireHttpsOrTerminateAttribute : RequireHttpsAttribute
+    {
+        protected override void HandleNonHttpsRequest(AuthorizationFilterContext filterContext)
+        {
+            filterContext.Result = new BadRequestResult();
+        }
+    }
+}
